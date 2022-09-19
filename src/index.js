ec1d9588305ba147ca49829940213805ffa54e8a
danted9822 <halaszpeter9814@gmail.com>
2022-09-19 21:39:57 +0200

    inital color bugfix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
- 
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+}
+
+const customTheme = extendTheme({ config })
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <ColorModeScript initialColorMode='dark'/>
-
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <App />
 
     </ChakraProvider>
   </React.StrictMode>
